44d803e3f1cbe895dc5cad2a0334b4ed1074b81c
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-02-25 01:10:39 +0530

    random

--- app.py
+++ app.py
@@ -62,10 +62,10 @@ def wit_parser(resp):
                 _source = resp['entities']['location'][0]['value']
                 _destination = resp['entities']['location'][1]['value']
                 if(_travel_mode == 'train'):
-                    return trains.train_between(_source, _destination, None), 200
+                    return trains.train_between(_source, _destination, None)
     except Exception as e:
-        return str(e), 200
-    return "Sorry! I'm not smart enough yet!", 200
+        return str(e)
+    return "Sorry! I'm not smart enough yet!"
 
 
 def send_message(recipient_id, message_text):
