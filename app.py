75e81d03b1833c52b5200ff578980614102a878f
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-02-25 03:35:35 +0530

    random

--- app.py
+++ app.py
@@ -75,7 +75,10 @@ def wit_parser(resp):
                     return trains.train_between(_source, _destination, None)
                 elif(_travel_mode == 'flight'):
                     _adults = resp['entities']['number'][0]['value']
-                    return flights.flight_between(_source, _destination,'20180225', _adults)
+                    print 'adults : ' + str(_adults)
+                    x = flights.flight_between(_source, _destination,'20180225', _adults)
+                    print x
+                    return x
     except Exception as e:
         return 'app.py error: ' + str(e)
     return "Sorry! I'm not smart enough yet!"
